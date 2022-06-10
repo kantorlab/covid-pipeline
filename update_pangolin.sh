e1231082fd4a56c031ff6b289fd5bf1fb526e41e
Mark Howison <mhowison@ripl.org>
2022-06-09 22:14:41 -0400

    new update_pangolin.sh

--- update_pangolin.sh
+++ update_pangolin.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -e
 VERSION=$1
-if [ -z $VERSION ]
+DATA=$2
+if [[ -z $VERSION || -z $DATA ]]
 then
-	echo "usage: bash update-pangoloin.sh NEW_PANGOLIN_VERSION"
+	echo "usage: bash update-pangolin.sh NEW_PANGOLIN_VERSION NEW_DATA_VERSION"
 	exit 1
 fi
 source /gpfs/data/rkantor/conda/bin/activate covid-v1
-pip uninstall -y pangolin pangoLEARN scorpio
+pip uninstall -y pangolin pangolin-data scorpio constellations
 pip install git+https://github.com/cov-lineages/scorpio.git
-pip install git+https://github.com/cov-lineages/pangoLEARN.git
+pip install git+https://github.com/cov-lineages/constellations.git
+pip install git+https://github.com/cov-lineages/pangolin-data.git@v${DATA}
 pip install git+https://github.com/cov-lineages/pangolin.git@v${VERSION}
 pangolin -v
 echo "Success!"
